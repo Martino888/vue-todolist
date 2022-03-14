1db6d52da68479847182c66bb7e2843f167c2fc0
Martino888 <maxmartino33@gmail.com>
2022-03-14 17:57:29 +0100

    todo-fine

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
+// Descrizione:
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+
+
 const app = new Vue({
     el: '#root',
     data: {
-        addTodos:'',
-        newTodo: '',
-        todos: [
-            'lavare',
-            'pulire',
-            'asciugare',
-        ]
+        stringa: '',
+        newTodo:{
+            text:'',
+            done:false,
+        },
+        todos:[{
+            text:'lista',
+            done: false,
+        }]
     },
     methods: {
-        deletetodo(index) {
-            this.todos.slice(index, 1);
-        },
         addtodos() {
             //il trim()toglie gli spazzi prima e dopo dalla stringa.
-            if (this.newTodo.trim() != '') {
-                this.todos.push(this.newTodo.trim());
-                this.newTodos = '';
-            }
+            if (this.stringa.trim() != '') {
+                this.newTodo.text = this.stringa.trim();
+                this.todos.push(this.newTodo);
+                this.stringa = '';
+                this.newTodo = {
+                    text:'',
+                    done:false,
+                }
+            } 
         }
-    }
+    },
 });
